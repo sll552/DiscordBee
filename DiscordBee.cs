fec9c966f5974b60f12c22e2d9444d739ab01e3b
Stefan Lengauer <slengauer522@gmail.com>
2021-11-01 00:37:38 +0100

    increase version to 2.0.0

--- DiscordBee.cs
+++ DiscordBee.cs
@@ -30,9 +30,9 @@ namespace MusicBeePlugin
       _about.Author = "Stefan Lengauer";
       _about.TargetApplication = "";   // current only applies to artwork, lyrics or instant messenger name that appears in the provider drop down selector or target Instant Messenger
       _about.Type = PluginType.General;
-      _about.VersionMajor = 1;  // your plugin version
-      _about.VersionMinor = 4;
-      _about.Revision = 2;
+      _about.VersionMajor = 2;  // your plugin version
+      _about.VersionMinor = 0;
+      _about.Revision = 0;
       _about.MinInterfaceVersion = MinInterfaceVersion;
       _about.MinApiRevision = MinApiRevision;
       _about.ReceiveNotifications = (ReceiveNotificationFlags.PlayerEvents | ReceiveNotificationFlags.TagEvents);
