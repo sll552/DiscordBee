5c205db9043aff02781976c920b897b014eaf84e
Stefan Lengauer <slengauer522@gmail.com>
2021-11-07 16:15:36 +0100

    Fix duration formatting

--- DiscordBee.cs
+++ DiscordBee.cs
@@ -150,7 +150,7 @@ namespace MusicBeePlugin
       ret.Add("PlayState", _mbApiInterface.Player_GetPlayState().ToString());
       ret.Add("Volume", Convert.ToInt32(_mbApiInterface.Player_GetVolume() * 100.0f).ToString());
       var duration = TimeSpan.FromMilliseconds(_mbApiInterface.NowPlaying_GetDuration());
-      ret.Add("Duration", $"{duration.Minutes}:{duration.Seconds}");
+      ret.Add("Duration", String.Format("{0:D}:{1:D2}", duration.Minutes, duration.Seconds));
 
       return ret;
     }
