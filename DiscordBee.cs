68d51a3e35a8a63f363a88f7f24edff582898550
sll552 <sll552@users.noreply.github.com>
2022-06-01 22:20:50 +0200

    Version 2.2.0 fixes #128

--- DiscordBee.cs
+++ DiscordBee.cs
@@ -33,7 +33,7 @@ namespace MusicBeePlugin
       _about.TargetApplication = "";   // current only applies to artwork, lyrics or instant messenger name that appears in the provider drop down selector or target Instant Messenger
       _about.Type = PluginType.General;
       _about.VersionMajor = 2;  // your plugin version
-      _about.VersionMinor = 1;
+      _about.VersionMinor = 2;
       _about.Revision = 0;
       _about.MinInterfaceVersion = MinInterfaceVersion;
       _about.MinApiRevision = MinApiRevision;
