f3186c6062943514003e82224519d5e3bd58f97f
Stefan Lengauer <slengauer522@gmail.com>
2022-07-17 16:01:03 +0200

    Version 3.0.3

--- DiscordBee.cs
+++ DiscordBee.cs
@@ -69,7 +69,7 @@ namespace MusicBeePlugin
       _about.Type = PluginType.General;
       _about.VersionMajor = 3;  // your plugin version
       _about.VersionMinor = 0;
-      _about.Revision = 2;
+      _about.Revision = 3;
       _about.MinInterfaceVersion = MinInterfaceVersion;
       _about.MinApiRevision = MinApiRevision;
       _about.ReceiveNotifications = ReceiveNotificationFlags.PlayerEvents;
