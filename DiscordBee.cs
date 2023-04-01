8f5b7a137ecbed7fa817ccf8e25f66760c38d6c2
Stefan Lengauer <slengauer522@gmail.com>
2023-04-01 21:56:58 +0200

    Version 3.1.0

--- DiscordBee.cs
+++ DiscordBee.cs
@@ -69,8 +69,8 @@ namespace MusicBeePlugin
       _about.TargetApplication = "";   // current only applies to artwork, lyrics or instant messenger name that appears in the provider drop down selector or target Instant Messenger
       _about.Type = PluginType.General;
       _about.VersionMajor = 3;  // your plugin version
-      _about.VersionMinor = 0;
-      _about.Revision = 4;
+      _about.VersionMinor = 1;
+      _about.Revision = 0;
       _about.MinInterfaceVersion = MinInterfaceVersion;
       _about.MinApiRevision = MinApiRevision;
       _about.ReceiveNotifications = ReceiveNotificationFlags.PlayerEvents;
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DiscordBee")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
